925649c18385d6d6d9b5137075379f815dd36fff
Karl Kilgi <kilgi.karl@gmail.com>
2013-06-27 23:00:44 +0300

    Added tests

--- test/functional/petitions_controller_test.rb
+++ test/functional/petitions_controller_test.rb
@@ -1,14 +1,48 @@
 require 'test_helper'
 
 class PetitionsControllerTest < ActionController::TestCase
-  test "should get index" do
+  setup do
+    @user = User.new
+    @user.name = 'Test'
+    @user.save
+
+    @petition = Petition.new
+    @petition.title = 'Test Petition'
+    @petition.body = 'Make the world a better place'
+    @petition.user_id = @user.id
+    @petition.target = 1
+    @petition.save
+  end
+
+  test 'should get index' do
     get :index
     assert_response :success
   end
 
-  test "should get mypetitions" do
-    get :mypetitions
+  test 'should get redirect' do
+    get :new
+    assert_response 302
+  end
+
+  test 'should show petition' do
+    get :show, id: @petition
     assert_response :success
+  end
+
+  test 'should get edit' do
+    get :edit, id: @petition
+    assert_response :success
+  end
+
+  test 'should destroy petition' do
+    assert_difference('Petition.count', -1) do
+      delete :destroy, id: @petition
+    end
+  end
+
+  test 'mypetitions should redirect' do
+    get :mypetitions
+    assert_response 302
   end
 
 end
--- test/unit/petition_test.rb
+++ test/unit/petition_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class PetitionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new
+    @user.name = 'Test'
+    @user.save
+
+    @petition = Petition.new
+    @petition.title = 'Test Petition'
+    @petition.body = 'Make the world a better place'
+    @petition.user_id = @user.id
+    @petition.target = 1
+    @petition.save
+  end
+
+  test 'Title is required' do
+    @petition.title = ''
+    assert !@petition.save
+  end
+
+  test '4 char title is not allowed' do
+    @petition.title = '1234'
+    assert !@petition.save
+  end
+
+  test 'Content is required' do
+    @petition.body = ''
+    assert !@petition.save
+  end
+
+  test '19 char content is not allowed' do
+    @petition.body = '1234567890123456789'
+    assert !@petition.save
+  end
+
+  test 'Target is required' do
+    @petition.target = ''
+    assert !@petition.save
+  end
+
+  test 'User is required' do
+    @petition.user_id = ''
+    assert !@petition.save
+  end
 end
